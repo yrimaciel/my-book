029d4bc09ea9e777fcac6f5d1be036fb54196839
Yuri Maciel <yurimpro@outlook.com>
2019-11-24 12:38:12 -0200

    adicionando o header e menu component e finalizando o header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
